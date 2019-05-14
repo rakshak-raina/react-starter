98de534059f294627f9ccdca70d84538f293f174
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-05-14 23:12:20 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import './styles/index.scss';
-import App from './scripts/App';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import './styles/index.scss';
 
+import App from './scripts/App';
+
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -4,7 +4,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <div className="inner">
+          <h1>DetecTify</h1>
+          <form action="10.12.143.251:8080/upload" method="post" encType="multipart/form-data">
+            <input type="file" name="brain" id="brain" />
+            <br/>
+            <button type="submit" value="Submit" className="btn btn-sm btn-primary">Submit</button>
+          </form>
+        </div>  
       </div>
     );
   }
